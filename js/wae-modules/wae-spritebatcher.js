76b8adcde99e118f5f0a7b4b4bc73fba5b2651f6
Caiyi Zhong <caiyz90@gmail.com>
2017-09-26 00:37:55 -0400

    aaaa

--- js/wae-modules/wae-spritebatcher.js
+++ js/wae-modules/wae-spritebatcher.js
@@ -11,18 +11,30 @@ define(
 		var batchList = [];
 		
 		var addToBatch = function (sprite) {
-			var f = sprite.getCurrentFrame();
-			var ssid = f.spriteSheet.ssid;
+			var frame = sprite.getCurrentFrame();
+            var x1 = sprite.position.x - frame.center.x;
+            var x2 = x1 + frame.width;
+            var y1 = sprite.position.y - frame.center.y;
+            var y2 = y1 + frame.height;
+			var ssid = frame.spriteSheet.ssid;
 			if (batchList[ssid]) {
 				// Append new vertices to batch
 			}
 			else {
-				// Create new batch
+				batchList[ssid] = {
+                    positions = [x1, y1, x2, y1, x1, y2, x2, y2],
+                    texCoords = [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0],
+                    indices = [0, 1, 2, 1, 2, 3]
+                }
 			}
 		};
         
-        var clear() {
+        var clear = function () {
             // Clear sprite batch
+        }
+        
+        var render = function () {
+            
         }
 	
     }
